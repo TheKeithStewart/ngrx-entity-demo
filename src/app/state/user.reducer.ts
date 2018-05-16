c01d57d5ed33d9bc882e3997f4b229309edb5667
Keith Stewart <kgstewart@gmail.com>
2018-05-16 14:33:20 -0500

    convert array to dictionary

--- src/app/state/user.reducer.ts
+++ src/app/state/user.reducer.ts
@@ -3,19 +3,36 @@ import { UserActions, UserActionTypes } from './user.actions';
 import { User } from '../models';
 
 export interface State {
-  users: User[];
+  users: {
+    ids: number[],
+    entities: {
+      [key: number]: User
+    }
+  };
 }
 
 export const initialState: State = {
-  users: []
+  users: {
+    ids: [],
+    entities: {}
+  }
 };
 
 export function reducer(state = initialState, action: UserActions): State {
   switch (action.type) {
     case UserActionTypes.LoadUsersSuccess: {
+      const ids = action.payload.users.map(user => user.id);
+      const entities: { [key: number]: User } = action.payload.users.reduce((map, obj) => {
+        map[obj.id] = obj;
+        return map;
+      }, {});
+
       return {
         ...state,
-        users: action.payload.users
+        users: {
+          ids,
+          entities
+        }
       };
     }
 
@@ -37,13 +54,10 @@ export const selectUsersState = createFeatureSelector<State>('user');
 
 export const selectAllUsers = createSelector(
   selectUsersState,
-  state => state.users
+  state => state.users.ids.map(id => state.users.entities[id])
 );
 
 export const selectUserById = (id: number) => createSelector(
   selectAllUsers,
-  users => {
-    const filteredUsers = users.filter(user => user.id === id);
-    return filteredUsers.length === 1 ? filteredUsers[0] : null;
-  }
+  users => users[id]
 );
